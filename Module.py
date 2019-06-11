6cfaa53a041c11471c0b960da58b8c021a5ee2f0
Wang Lida <wanglida6@hotmail.com>
2019-06-11 09:17:46 +0800

    学习调用Python系统模块

--- Module.py
+++ Module.py
@@ -1,13 +1,13 @@
-#==模块，使用Python标准库的方法==
+# 模块，使用Python标准库的方法
 
-  #使用sys模块
+  # 使用sys模块
 import sys
 print('The command line arguments are:')
 for i in sys.argv:
     print(i)
 print('\nThe PYTHON PATH is', sys.path, '\n')
 
-  #使用模块的__name__
+  # 使用模块的__name__
 if __name__=='__main__':
     print('This program is being run by itself.')
 else:
@@ -16,5 +16,5 @@ import PersonalModule
 PersonalModule.firstMoudule()
 print('Module Version=',PersonalModule.version)
 
-print(dir(sys))   #列出模块定义的标识符
+print(dir(sys))   # 列出模块定义的标识符
 
