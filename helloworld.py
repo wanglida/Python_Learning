f6e6d3ac64388e48570e45cd4ffd21d99eb958f0
Wang Lida <wanglida6@hotmail.com>
2019-09-01 20:07:00 +0800

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -18,23 +18,32 @@ elif num==5:
     print("该数等于5，num=",num)
 else:
     print("概述大于5：num=",num)
+    
+
 # while循环
 res=0
 while res<9:
     print('The result is :',res)
     res+=1
+
+
 # for 循环
 for letter in "IamIronMan":
     print("当前字母：",letter)
 AllFruits=['苹果','香蕉','桃子','葡萄','others']
 for fruit in AllFruits:
     print('当前水果:',fruit)
+    
+    
 # 定义函数
 def sayHello(a,b,c="末尾"):  # 只有在形参表末尾的那些参数可以有默认参数值
     global x
     x=6
     print('Hello! The fuction!a=',a,'b=',b,'c=',c)
     print('函数内x=',x)
+    
+    
+
 # 调用函数
 x=8
 sayHello(3,4)
